fbabc6a1928143be204fec8b9c8c535d7bf6180c
96RameshBhandari <96rameshbhandari@gmail.com>
2024-09-12 00:52:57 +0545

    Changing Table And Adding Search

--- DGCloud.UI/src/app/reusable/components/table/table.component.ts
+++ DGCloud.UI/src/app/reusable/components/table/table.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output, OnChanges, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
   @Input() tableData:any[]=[];
   @Input() columnArray:any[]=[];
   @Input() isActionTable:boolean=false;
@@ -16,6 +17,8 @@ export class TableComponent {
   @Output() onEdit= new EventEmitter<any>();
   @Output() onDelete= new EventEmitter<any>();
 
+  searchedText:string='';
+  filteredData:any[]=[];
 
   columnKeys:string[]=[];
 
@@ -27,5 +30,15 @@ export class TableComponent {
   }
   onDeleteRecord(item:any){
     this.onDelete.emit(item);
+  }
+  onSearchChange(searchVal:string){
+    console.log('trigger', searchVal);
+    this.filteredData = this.tableData.filter((searchData: any) => {
+      const values = Object.values(searchData);
+      return values.some((val: any) => val.toString().toLowerCase().indexOf(searchVal.toLowerCase()) > -1);
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.filteredData=this.tableData;
   }
 }
